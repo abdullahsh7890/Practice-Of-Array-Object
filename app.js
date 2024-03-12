4cd85525dc0701511cfcdba767e667f08b2bfb7f
abdullahsh7890 <154223491+abdullahsh7890@users.noreply.github.com>
2024-03-12 20:26:58 +0500

    Array&Objects

---
+++ app.js
@@ -0,0 +1,20 @@
+ // Objects
+ 
+var details = { // in this step we declear object
+    name: "Abdullah",
+    class: "Xii-A" ,
+}
+console.log(details.class) // in this step we accessing object by using .keyname in this line class is a key
+console.log(details.name)
+
+// Arrays
+// Arrays index value start from 0 and length always start with 1
+
+var id =["Abdullah",1,2,3,4] // in this step we store data in array reference
+// in this step we accessing array data by using index value
+id.push("I am genius")
+console.log(id) 
+id.pop(0)
+console.log(id)
+
+
